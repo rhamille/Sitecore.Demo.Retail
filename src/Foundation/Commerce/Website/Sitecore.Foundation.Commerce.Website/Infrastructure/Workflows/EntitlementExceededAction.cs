2b8d389c72145034120c3d41dd02c5b903e8577d
joel.reade <joel.reade@JOEL-LAPTOP>
2017-09-22 12:08:35 +1000

    update send email functionality for workflow.

--- src/Foundation/Commerce/Website/Sitecore.Foundation.Commerce.Website/Infrastructure/Workflows/EntitlementExceededAction.cs
+++ src/Foundation/Commerce/Website/Sitecore.Foundation.Commerce.Website/Infrastructure/Workflows/EntitlementExceededAction.cs
@@ -27,4 +27,59 @@ namespace Sitecore.Foundation.Commerce.Website.Infrastructure.Workflows
         }
 
     }
+
+    public class EntitlementExceededActionEmail
+    {
+        public void Process(WorkflowPipelineArgs args)
+        {
+
+            Assert.ArgumentNotNull((object)args, "args");
+            ProcessorItem processorItem = args.ProcessorItem;
+            if (processorItem == null)
+                return;
+            Item innerItem = processorItem.InnerItem;
+            string fullPath = innerItem.Paths.FullPath;
+            string text1 = this.GetText(innerItem, "from", args);
+            string text2 = this.GetText(innerItem, "to", args);
+            string text3 = this.GetText(innerItem, "mail server", args);
+            string text4 = this.GetText(innerItem, "subject", args);
+            string text5 = this.GetText(innerItem, "message", args);
+            text5 = text5.Replace("{orderid}", args.DataItem["OrderId"]);
+            Error.Assert(text2.Length > 0, "The 'To' field is not specified in the mail action item: " + fullPath);
+            Error.Assert(text1.Length > 0, "The 'From' field is not specified in the mail action item: " + fullPath);
+            Error.Assert(text4.Length > 0, "The 'Subject' field is not specified in the mail action item: " + fullPath);
+            Error.Assert(text3.Length > 0, "The 'Mail server' field is not specified in the mail action item: " + fullPath);
+            new SmtpClient(text3).Send(new MailMessage(text1, text2)
+            {
+                Subject = text4,
+                Body = text5
+            });
+        }
+
+        /// <summary>Gets the text.</summary>
+        /// <param name="commandItem">The command item.</param>
+        /// <param name="field">The field.</param>
+        /// <param name="args">The arguments.</param>
+        /// <returns></returns>
+        private string GetText(Item commandItem, string field, WorkflowPipelineArgs args)
+        {
+            string text = commandItem[field];
+            if (text.Length > 0)
+                return this.ReplaceVariables(text, args);
+            return string.Empty;
+        }
+
+        /// <summary>Replaces the variables.</summary>
+        /// <param name="text">The text.</param>
+        /// <param name="args">The arguments.</param>
+        /// <returns></returns>
+        private string ReplaceVariables(string text, WorkflowPipelineArgs args)
+        {
+            text = text.Replace("$itemPath$", args.DataItem.Paths.FullPath);
+            text = text.Replace("$itemLanguage$", args.DataItem.Language.ToString());
+            text = text.Replace("$itemVersion$", args.DataItem.Version.ToString());
+            return text;
+        }
+
+    }
 }
